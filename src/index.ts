07eec9653512b0c0a1d55a8e510ba91397f37f01
Simon Dell <simon.dell@founders4schools.org.uk>
2020-05-03 19:37:02 +0100

    Naive reimplementation of redux/useReducer

--- src/index.ts
+++ src/index.ts
@@ -48,19 +48,104 @@ function degreesToRadians (angle: number): number {
 	return angle * Math.PI / 180
 }
 
-const state = {
-	Rocket: {
-		angle: 60,
-		position: {
-			x: canvas.width / 2,
-			y: canvas.height / 2,
-		}
+
+// store ///////////////////////////////////////////////////////////////////////
+interface Action {
+	error?: Error
+	payload?: any
+	type: string
+}
+
+const defaultRocket = {
+	angle: 60,
+	position: {
+		x: canvas.width / 2,
+		y: canvas.height / 2,
 	}
 }
 
+// interface Reducer<T> {
+// 	state: any
+// 	action: Action
+// }
 
+function rocket (
+	state = defaultRocket,
+	action?: Action
+) {
+	return state
+}
+
+// interface Selector<T> {
+// 	(s: Store): T
+// }
+
+function getRocket (state: Store): Rocket {
+	return state.rocket
+}
+
+const [store, dispatch] = createStore({ rocket })
+
+interface ReducerMap {
+	[key: string]: Function
+}
+
+interface Store {
+	[key: string]: any
+}
+
+interface Dispatch {
+	(a: Action): void
+}
+
+// naive `composeReducers`/`useReducer`
+
+function createStore (reducerMap: {[key: string]: Function}): [Store, Dispatch] {
+	const createAction: Action = { type: 'CREATE' }
+	const sliceNames = Object.keys(reducerMap)
+	const store = sliceNames.reduce(getDefaultState, {} as Store)
+
+	return [store, dispatch]
+
+	function getDefaultState (store: Object, sliceName: string): Object {
+		return {
+			...store,
+			[sliceName]: reducerMap[sliceName](undefined, createAction)
+		}
+	}
+
+	function dispatch(action: Action): void {
+		sliceNames.forEach((sliceName: string) => {
+			store[sliceName] = reducerMap[sliceName](store[sliceName], action)
+		})
+	}
+}
+////////////////////////////////////////////////////////////////////////////////
+
+// controls ////////////////////////////////////////////////////////////////////
+enum CONTROLS {
+	LEFT = 37,
+	// POWER = 81,
+	RIGHT = 39,
+	// SHOOT = 32,
+	// UP = 38,
+}
+
+function onKeyDown (event: KeyboardEvent): void {
+	switch(event.keyCode) {
+		case CONTROLS.LEFT:
+			dispatch({ type: 'ROTATE/LEFT' })
+			break;
+	}
+}
+
+document.addEventListener('keydown', onKeyDown)
+////////////////////////////////////////////////////////////////////////////////
+
+// game loop ///////////////////////////////////////////////////////////////////
 function draw (): void {
-	renderRocket( context, state.Rocket )
+	renderRocket( context, getRocket(store) )
 }
 
 const loop = requestAnimationFrame(draw)
+////////////////////////////////////////////////////////////////////////////////
