d7c364aba9381f4a5be3d0bfa07a27a4b603d9f9
Simon Dell <simon.dell@founders4schools.org.uk>
2020-05-10 01:21:42 +0100

    Add series and parallel combinators; new createStore expects single reducer

--- src/store.ts
+++ src/store.ts
@@ -32,9 +32,12 @@ export interface Selector<T> {
 	( s: Store ): T
 }
 
-export interface Store {
+interface Mapable {
 	[ key: string ]: any
 }
+
+export type Store = Mapable
+// export type Store = number | string | Array<any> | Mapable
 
 export function createAction ( type: string ): Function {
 	return function ( payload?: any ): Action {
@@ -45,17 +48,100 @@ export function createAction ( type: string ): Function {
 	}
 }
 
+function combineInParallel (
+	reducerMap: ReducerMap
+): Reducer<ReducerMap> {
+	return function reduceInParallel ( state: Mapable, action: Action ): ReducerMap {
+		const sliceNames = Object.keys(reducerMap)
+
+		function reduceSlices (state: Mapable, sliceName: string): Mapable {
+			const sliceState = state[sliceName]
+			const reducer = reducerMap[sliceName]
+			const nextSliceState = reducer(sliceState, action)
+
+			if( nextSliceState == sliceState ) return state
+
+			return {
+				...state,
+				[sliceName]: nextSliceState
+			}
+		}
+
+		return sliceNames.reduce(reduceSlices, state)
+	}
+}
+
+function combineInSeries (...reducers: Reducer<any>[] ): Reducer<any> {
+	return function reduceInSeries ( state: Store, action: Action ): Store {
+		function reduceReducers (state: Store, reducer: Reducer<any>) {
+			return reducer(state, action)
+		}
+
+		return reducers.reduce(reduceReducers, state)
+	}
+}
+
+interface GetState {
+	(): Store
+}
+
+interface Listener {
+	(s: Store): void
+}
+
+export function createStore2 (
+	rootReducer: Reducer<any>
+): [GetState, Dispatch, Notify] {
+	let state: any
+	let subscriptions: Function[] = []
+
+	// interface ActionCreator {
+	// 	(a: string): Action
+	// }
+
+	// function bindToDispatch (actionCreator: ActionCreator) {
+	// 	const action = 
+	// 	return dispatch( actionCreator() )
+	// }
+
+	function dispatch(action: Action): void {
+		state = rootReducer(state, action)
+		subscriptions.forEach(subscripton => { subscripton(state) })
+	}
+
+	function getState (): Store {
+		return state
+	}
+
+	function notify (listener: Listener, shouldInvokeImmediate = true) {
+		const length = subscriptions.push(listener)
+
+		if( shouldInvokeImmediate ) {
+			listener(state)
+		}
+
+		return function () {
+			subscriptions = [
+				...subscriptions.slice(0, length - 1),
+				...subscriptions.slice(length)
+			]
+		}
+	}
+
+	return [getState, dispatch, notify]
+}
+
 // naive `composeReducers`/`useReducer`
 export function createStore (
 	reducerMap: ReducerMap
-): [Store, Dispatch, Notify] {
+): [Mapable, Dispatch, Notify] {
 	const init: Action = { type: 'STORE/INITIALISE' }
 	const sliceNames = Object.keys(reducerMap)
 
-	const store = sliceNames.reduce(getDefaultState, {} as Store)
+	const store = sliceNames.reduce(getDefaultState, {} as Mapable)
 	let subscriptions: Function[] = []
 
-	function getDefaultState (store: Object, sliceName: string): Object {
+	function getDefaultState (store: Mapable, sliceName: string): Mapable {
 		return {
 			...store,
 			[sliceName]: reducerMap[sliceName](undefined, init)
