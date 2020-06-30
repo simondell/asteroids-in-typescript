66b2e729c7bd7dc9d18854517b61b9def26b7b62
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-30 17:27:08 +0100

    Change code style: arrow functions can appear on the same line

--- src/game.test.ts
+++ src/game.test.ts
@@ -26,32 +26,22 @@ const triangleAsteroid = {
 	velocity: { x: 5, y: -5 },
 }
 
-describe(
-	'asteroids',
-	() => {
-		test.each([
-			[squareAsteroid, { x: 1, y: 2 }],
-			[triangleAsteroid, { x: 28, y: 37 }]
-		])(
-			'ticks should move %o to %j',
-			(roid, expectedPos) => {
-				const moved = game.asteroids([roid], game.tick())
+describe('asteroids', () => {
+	test.each([
+		[squareAsteroid, { x: 1, y: 2 }],
+		[triangleAsteroid, { x: 28, y: 37 }]
+	])('ticks should move %o to %j', (roid, expectedPos) => {
+		const moved = game.asteroids([roid], game.tick())
 
-				expect(moved[0].position).toEqual(expectedPos)
-			}
-		)
-	}
-)
+		expect(moved[0].position).toEqual(expectedPos)
+	})
+})
 
-describe(
-	'when bullets collide with asteroids',
-	() => {
+describe('when bullets collide with asteroids', () => {
 		let beforeTick: game.GameState
 		let afterTick: game.GameState
 
-		describe(
-			'when the rocket has not fired',
-			() => {
+		describe('when the rocket has not fired', () => {
 				beforeEach(
 					() => {
 						const asteroids = [
@@ -69,26 +59,20 @@ describe(
 						afterTick = game.damageAsteroids( beforeTick, game.tick() )
 					}
 				)
-				test(
-					'expect no change to the asteroids',
-					() => {
+				test('expect no change to the asteroids', () => {
 						expect( afterTick.asteroids ).toContain( squareAsteroid )
 						expect( afterTick.asteroids ).toContain( triangleAsteroid )
 					}
 				)
 
-				test(
-					'expect no change to the bullets',
-					() => {
+				test('expect no change to the bullets', () => {
 						expect( afterTick.bullets ).toEqual( [] )
 					}
 				)
 			}
 		)
 
-		describe(
-			'when the rocket has fired',
-			() => {
+		describe('when the rocket has fired', () => {
 				const hits = {
 					position: { x: 22, y: 42 },
 					velocity: { x: 10, y: 0 },
@@ -99,9 +83,7 @@ describe(
 					velocity: { x: 0, y: 10 },
 				}
 
-				describe(
-					'and when the bullet misses all the asteroids',
-					() => {
+				describe('and when the bullet misses all the asteroids', () => {
 						beforeEach(
 							() => {
 								const asteroids = [
@@ -122,26 +104,20 @@ describe(
 							}
 						)
 
-						test(
-							'retain all the asteroids',
-							() => {
+						test('retain all the asteroids', () => {
 								expect( afterTick.asteroids ).toContain( squareAsteroid )
 								expect( afterTick.asteroids ).toContain( triangleAsteroid )
 							}
 						)
 
-						test(
-							'retain the bullet',
-							() => {
+						test('retain the bullet', () => {
 								expect( afterTick.bullets ).toContain( misses )
 							}
 						)
 					}
 				)
 
-				describe(
-					'and when the bullet hits an asteroid',
-					() => {
+				describe('and when the bullet hits an asteroid', () => {
 						beforeEach(
 							() => {
 								const asteroids = [
@@ -162,23 +138,17 @@ describe(
 							}
 						)
 
-						test(
-							'remove the asteroid',
-							() => {
+						test('remove the asteroid', () => {
 								expect( afterTick.asteroids ).not.toContain( triangleAsteroid )
 							}
 						)
 
-						test(
-							'retain the other asteroid',
-							() => {
+						test('retain the other asteroid', () => {
 								expect( afterTick.asteroids ).toContain( squareAsteroid )
 							}
 						)
 
-						test(
-							'remove the bullet',
-							() => {
+						test('remove the bullet', () => {
 								expect( afterTick.bullets ).not.toContain( hits )
 							}
 						)
@@ -186,66 +156,62 @@ describe(
 				)
 			}
 		)
-	}
-)
+})
 
-describe(
-	'asteroidsSmashRockets()',
-	() => {
-		test(
-			'when rocket and asteroid collide the rocket gets nixed',
-			() => {
-				const rocketKillerAsteroid = {
-					testId: 'rocket-killer',
-					points: [
-						{ x: 0, y: 0 },
-						{ x: 0, y: -1 },
-						{ x: 1, y: -1 },
-						{ x: 1, y: 0 },
-					],
-	        position: { x: 420, y: 280 },
-					radius: 45,
-					velocity: { x: 0, y: 1 },
-				}
-
-				const stateBefore = {
-					...mockState,
-					asteroids: [
-						squareAsteroid,
-						triangleAsteroid,
-						rocketKillerAsteroid,
-					]
-				}
-
-				const stateAfter = game.asteroidsSmashRockets( stateBefore, game.tick() )
-
-				expect( stateAfter.rocket ).toEqual( null )
+describe('when rockets and asteroids collide', () => {
+	test('collisions nix the rocket', () => {
+			const rocketKillerAsteroid = {
+				testId: 'rocket-killer',
+				points: [
+					{ x: 0, y: 0 },
+					{ x: 0, y: -1 },
+					{ x: 1, y: -1 },
+					{ x: 1, y: 0 },
+				],
+        position: { x: 420, y: 280 },
+				radius: 45,
+				velocity: { x: 0, y: 1 },
 			}
-		)
 
-		test(
-			'when rocket and asteroid don\'t collide, no state changes',
-			() => {
-				const stateBefore = {
-					...mockState,
-					asteroids: [
-						squareAsteroid,
-						triangleAsteroid,
-					]
-				}
-
-				const stateAfter = game.asteroidsSmashRockets( stateBefore, game.tick() )
-
-				expect( stateAfter.rocket ).toEqual( stateBefore.rocket )
+			const stateBefore = {
+				...mockState,
+				asteroids: [
+					squareAsteroid,
+					triangleAsteroid,
+					rocketKillerAsteroid,
+				]
 			}
-		)
 
-		// test(
-		// 	'the lives count reduces by 1',
-		// 	() => {
-				
-		// 	}
-		// )
-	}
-)
+			const stateAfter = game.asteroidsSmashRockets( stateBefore, game.tick() )
+
+			expect( stateAfter.rocket ).toEqual( null )
+	})
+
+	test('misses have no effect', () => {
+			const stateBefore = {
+				...mockState,
+				asteroids: [
+					squareAsteroid,
+					triangleAsteroid,
+				]
+			}
+
+			const stateAfter = game.asteroidsSmashRockets( stateBefore, game.tick() )
+
+			expect( stateAfter.rocket ).toEqual( stateBefore.rocket )
+	})
+
+	// test(
+	// 	'the lives count reduces by 1',
+	// 	() => {
+			
+	// 	}
+	// )
+})
+
+describe('scoring', () => {
+	test('set to zero when game starts', () => {
+
+	})
+})
 ////////////////////////////////////////////////////////////////////////////////
