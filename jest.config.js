48fee19a5a5ea509f0e1dc81175cf3393bc7208d
Simon Dell <simon.dell@dazn.com>
2025-02-02 00:52:56 +0000

    refactor: update deps and config to lay the groundwork for a ci/cd pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^/src/(.*)\.js$': '<rootDir>/src/$1.ts',
-    '^/src/libs/(.*)\.js$': '<rootDir>/src/libs/$1.ts',
-    '^\./libs/(.*)\.js$': '<rootDir>/src/libs/$1.ts',
-  }
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
 };
